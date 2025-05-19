091cb7020ee0d7605ec38a601b9166906fc52b14
Dashctlab <dashct@gmail.com>
2025-05-19 16:20:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
   backendURL: typeof window !== 'undefined' 
-    ? window.ENV?.BACKEND_URL || process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-rivu-vercel-render-backend.onrender.com'
+    ? process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-rivu-vercel-render-backend.onrender.com'
     : process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-rivu-vercel-render-backend.onrender.com'
 };
 
