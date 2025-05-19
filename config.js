99ed64d5d3bccce00edaf54c1c2d4613f298cc15
Dashctlab <dashct@gmail.com>
2025-05-19 20:38:17 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const config = {
-  backendURL: typeof window !== 'undefined' 
-    ? process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-rivu-vercel-render-backend.onrender.com'
-    : process.env.NEXT_PUBLIC_BACKEND_URL || 'https://ai-rivu-vercel-render-backend.onrender.com'
-};
-
-export default config;
