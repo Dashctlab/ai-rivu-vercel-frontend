ca0fb5b559d84b509be588fea778141ef8a7bd03
Dashctlab <dashct@gmail.com>
2025-05-23 17:11:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 // this lives in the web root and must be loaded before any other script
 window.APP_CONFIG = {
-  BACKEND_URL: 'https://ai-rivu-backend-staging.onrender.com'
+  BACKEND_URL: 'https://ai-rivu-vercel-render-backend-staging.onrender.com/'
 };
