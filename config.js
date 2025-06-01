41e803078628ca559f6d130573ca8389bce7452c
Dashctlab <dashct@gmail.com>
2025-06-01 21:35:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // config.js
-require('dotenv').config();
 window.APP_CONFIG = {
-  BACKEND_URL: `${BACKEND_URL}` // Gets replaced during deployment
+  BACKEND_URL: '__BACKEND_URL__' // Placeholder
 };
